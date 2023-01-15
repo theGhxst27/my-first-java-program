500687c9eca9f56253392229e486c6bae132625e
oreo748 <ooferoof800@gmail.com>
2023-01-15 13:00:54 -0500

    0.1.2
    holy shit i forgot \n works in java lmao

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
         //prints
-        System.out.println("Hello world!");
-        System.out.println("what is your name?");
+        System.out.println("Hello world! \nwhat is your name?");
         
         // defines scanner
         System.out.print("INPUT: ");
@@ -18,19 +17,14 @@ public class Main {
         String name = scn.nextLine(); // pauses code
         
         // prints the story
-        System.out.println("Hello " + name + "!");
-        System.out.println("Oh no! An enemy blocks your path! What do you do?");
-        System.out.println("=========================================");
+        System.out.println("Hello " + name + "! \nOh no! An enemy blocks your path! What do you do? \n=========================================");
         battle("Weak Enemy");
     }
 
     public static void battle(String name){
         // boring dialoge nobody cares
-        System.out.println("Your options are a (attack) or r (run)");
-        System.out.println("=========================================");
-        System.out.println("Your Health: " + usrH + "| Your Attack: " + usrA);
-        System.out.println(name + "'s Health: " + enmH + " | " + name + "'s Attack: " + enmA);
-        System.out.println("=========================================");
+        System.out.println("Your options are a (attack) or r (run) \n========================================= \nYour Health: " + usrH + "| Your Attack: " + usrA);
+        System.out.println(name + "'s Health: " + enmH + " | " + name + "'s Attack: " + enmA + "\n=========================================");
         
         // the fun part (your attack)
         Scanner scn = new Scanner(System.in);
@@ -38,11 +32,7 @@ public class Main {
         String chosenInput = scn.nextLine(); // pauses code
 
         if (chosenInput.equals("a")) { // you chose attack
-            System.out.println("You chose attack!");
-            System.out.println("=========================================");
-            System.out.println("press 1 for a low blow (attack + 50%, but 50% chance to fail)");
-            System.out.println("press 2 for a high blow (attack + 0%, but 0% chance to fail)");
-            System.out.println("=========================================");
+            System.out.println("You chose attack! \n========================================= \npress 1 for a low blow (attack + 50%, but 50% chance to fail) \npress 2 for a high blow (attack + 0%, but 0% chance to fail) \n=========================================");
             System.out.print("INPUT: ");
             String chosenAttack = scn.nextLine(); // pauses code
             if (chosenAttack.equals("1")){
@@ -60,11 +50,9 @@ public class Main {
         } if (chosenInput.equals("r")) { // you chose run
             double rand_doub1 = Math.random();
             if (rand_doub1 >= 0.5){
-                System.out.println("It works!");
-                System.out.println("You swallow your pride and escape unharmed");
+                System.out.println("It works! \nYou swallow your pride and escape unharmed");
             } else {
-                System.out.println("It fails!");
-                System.out.println("The enemy attacks you for your cowardice!");
+                System.out.println("It fails! \nThe enemy attacks you for your cowardice!");
                 updateHealth(10, 0);
             }
         }
